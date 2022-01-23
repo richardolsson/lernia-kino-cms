01f18384d87c3ca3af4de2ade7bcd681c8c5da54
Richard Olsson <r@richardolsson.se>
2022-01-23 11:25:32 +0100

    Add screening content type

---
+++ src/api/screening/controllers/screening.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  screening controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::screening.screening');
